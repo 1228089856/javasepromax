66c51b5072989cea98fd83c721fa25e770e17e3e
1228089856 <1228089856@qq.com>
2023-01-05 22:40:58 +0800

    first commit

--- day01-oop-demo/src/static_code/StaticDemo1.java
+++ day01-oop-demo/src/static_code/StaticDemo1.java
@@ -5,7 +5,7 @@ package static_code;
 public class StaticDemo1 {
     /**
      * 静态代码块：有static修饰，属于类，与类一起优先加载一次，自动触发执行
-     * 作用：可以用于吃石化静态资源
+     * 作用：可以用于初始化静态资源
      */
     static {
         System.out.println("============静态代码块被触发执行了===========");
