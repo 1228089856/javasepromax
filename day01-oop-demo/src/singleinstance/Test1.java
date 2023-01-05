3b947413a3dd221388ad4c7374aec4737422a33c
1228089856 <1228089856@qq.com>
2023-01-05 23:22:01 +0800

    first commit

---
+++ day01-oop-demo/src/singleinstance/Test1.java
@@ -0,0 +1,8 @@
+package singleinstance;
+
+public class Test1 {
+    public static void main(String[] args) {
+        //目标：理解饿汉单例的设计步骤
+
+    }
+}
---
+++ day01-oop-demo/src/singleinstance/Test2.java
@@ -0,0 +1,7 @@
+package singleinstance;
+
+public class Test2 {
+    public static void main(String[] args) {
+        //目标：掌握懒汉单例的设计。理解其思想
+    }
+}
