01e8792f0c4634303a58669d02b5f7daaaaa6098
1228089856 <1228089856@qq.com>
2023-01-06 00:47:16 +0800

    first commit

---
+++ day01-oop-demo/src/extend/Student.java
@@ -0,0 +1,7 @@
+package extend;
+
+/**
+ * 学生类：子类
+ */
+public class Student extends People {
+}
---
+++ day01-oop-demo/src/extend/Test.java
@@ -0,0 +1,9 @@
+package extend;
+
+public class Test {
+    public static void main(String[] args) {
+        //目标：认识集成这种关系，搞清楚使用集成的好处
+        Student s = new Student();
+        s.run();
+    }
+}
