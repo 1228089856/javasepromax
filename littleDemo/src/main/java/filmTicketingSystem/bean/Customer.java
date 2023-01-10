4e2171a98b7b24e3727e551499d938b92816fc6b
1228089856 <1228089856@qq.com>
2023-01-10 23:10:20 +0800

    first commit

---
+++ littleDemo/src/main/java/filmTicketingSystem/bean/Customer.java
@@ -0,0 +1,12 @@
+package filmTicketingSystem.bean;
+
+import java.util.Map;
+
+/**
+ * 客户角色
+ */
+public class Customer extends User{
+    public Map<String, Boolean> getBuyMovies() {
+        return null;
+    }
+}
